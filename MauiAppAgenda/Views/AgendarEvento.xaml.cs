4c56c387d506d14804c2e709875e9ba213a07aec
IzaiasCF <izaiascorreafilho@gmail.com>
2025-05-29 12:00:30 -0300

    add arquivo

--- MauiAppAgenda/Views/AgendarEvento.xaml.cs
+++ MauiAppAgenda/Views/AgendarEvento.xaml.cs
@@ -6,4 +6,24 @@ public partial class AgendarEvento : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void dtpck_inicio_DateSelected(object sender, DateChangedEventArgs e)
+    {
+        // Exemplo: exibe no console a nova data selecionada
+        DateTime novaData = e.NewDate;
+        Console.WriteLine($"Data de início selecionada: {novaData:dd/MM/yyyy}");
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new Evento());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Ops!", ex.Message, "Fechar");
+        }
+         
+    }
 }
